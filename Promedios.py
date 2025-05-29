6741787bd4a6c4ee86b4afb4739b613571c37b99
Andy20s-75 <anaandread408@gmail.com>
2025-05-29 17:21:47 -0600

    Se añadio funcion calcular_promedios

--- Promedios.py
+++ Promedios.py
@@ -1,14 +1,25 @@
-def obtener_num(mensaje): #Funcion para obtener valores enteros validos
-    valido=False
-    while valido==False:
+def obtener_num(mensaje, minimo=1, maximo=None):  # Funcion para obtener valores enteros validos
+    valido = False
+    while not valido:
         entrada = input(mensaje)
-        if entrada.isdigit():  #Verifica si la entrada es un numero
+        if entrada.isdigit():  # Verifica si la entrada es un número
             valor = int(entrada)
-            if 1 <= valor:
+            if valor >= minimo and (maximo is None or valor <= maximo):
                 return valor
-            print("Error: Debe ser mayor a 1")
+            else:
+                if maximo is not None:
+                    print(f"Error: El número debe estar entre {minimo} y {maximo}")
+                else:
+                    print(f"Error: El número debe ser mayor o igual a {minimo}")
         else:
-            print("Error: Ingrese un numero entero valido")
+            print("Error: Ingrese un número entero válido")
+
+def calcular_promedio(lista):
+    suma = 0
+    for i in range(len(lista)):
+        suma += lista[i]
+    promedio = suma / len(lista)
+    return promedio
 
 # Programa principal
 estudiantes=[]
@@ -28,18 +39,21 @@ for i in range(num_estudiantes):
             print("Error: El nombre no puede estar vacío")
         else:
             break
+
+    calificaciones = [] #Reinicia la calif. por estudiante    
     
     for j in range(3):
         calificacion = obtener_num(f"Calificación Materia {j + 1} (0-10): ", 0, 10)
         calificaciones.append(calificacion)
     
-    #promedio = calcular_promedio(calificaciones)
+    promedio = calcular_promedio(calificaciones)
     #estado = evaluar_aprobacion(promedio)
 
     estudiantes.append({ #se usa un diccionario para estudiantes
         "nombre": nombre,
-        #"promedio": promedio,
-        #"estado": estado
+        "promedio": promedio,
+        "estado": "Aprobado"
+        if promedio >= 6 else "Reprobado"
     })
 
 #Mostrar resultados
